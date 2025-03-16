bdb5395835c636adda378accb8ce113269bf04a8
Péter Vida <peter.von.vida@gmail.com>
2025-03-16 23:52:15 +0100

    adjust readme and smarten up component

--- README.md
+++ README.md
@@ -1,31 +1,38 @@
 # `<script-ts>`
 A web component to be able to use typescript in simple web pages.
 
-After seeing the announcement for [--eraseableSyntaxOnly in typescript 5.8](https://devblogs.microsoft.com/typescript/announcing-typescript-5-8/#the---erasablesyntaxonly-option) it gave me the idea that maybe node should not be the only one capable of such feats.
+After seeing the announcement for 
+[--eraseableSyntaxOnly in typescript 5.8](https://devblogs.microsoft.com/typescript/announcing-typescript-5-8/#the---erasablesyntaxonly-option)
+it gave me the idea that maybe node should not be the only one capable of such
+feats.
 
-**Current features:**
-- [x] uses [ts-blank-space](https://github.com/bloomberg/ts-blank-space) to replace types with whitespace
+**"roadmap"**
+- [x] use [ts-blank-space](https://github.com/bloomberg/ts-blank-space) to
+      replace types with whitespace
 - [ ] pass attributes to the underlying script tag
-- [ ] figure out distribution + licensing
 - [ ] reduce bundle size
+- [ ] figure out distribution
+- [ ] syntax highlighting on github pages
 
 ## Usage
-Load the web component and use `<script-ts>` just like you would the regular `<script>` tag.
+Load the web component by grabbing the bundle and use `<script-ts>` just like 
+the regular `<script>` tag.
 
 ```html
 <script src="https://cdn.jsdelivr.net/gh/petii/script-ts/script-ts.min.js"></script>
 ...
 <script-ts>
   // while inlining typescript technically works there's not a lot of support for it
-  // in IDEs or syntax highlight
   const foo: string = 'bar';
 </script-ts>
-<!-- loading files however could help with development -->
+<!-- loading files directly means that development can be easier -->
 <script-ts src="you-script.ts"></script-ts>
 ```
 
 > [!CAUTION]
-> To get typescript to play along nicely the component removes (comments out) `import` statements which could have unintended side effects.
+> To get tsc to play along nicely, the component removes (comments out) `import` statements which could have unintended side effects.
+
+`<script-ts>` can emit log messages if the `script-ts-log=enable` query param is set.
 
 ## Development
 ~~Unfortunately it's not as easy as loading up the file but serving it locally should work: `npx serve --cors`~~
--- script-ts.ts
+++ script-ts.ts
@@ -4,32 +4,100 @@ function setTimeoutAsync(ms?: Parameters<typeof setTimeout>[1]) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-class ScriptTs extends HTMLElement {
+export class ScriptTs extends HTMLElement {
+  static observedAttributes: string[] = ["src"];
+
+  public originalSource: string;
+  public actualScript: HTMLScriptElement;
+
   constructor() {
     super();
   }
 
+  log(
+    level: Extract<
+      keyof typeof console,
+      "log" | "debug" | "error" | "trace" | "warn" | "info"
+    >,
+    ...args: unknown[]
+  ) {
+    // silence console messages unless explicitly asked for
+    if (!location.search?.includes("script-ts-log=enable")) {
+      return;
+    }
+
+    const maybeId = this.attributes.getNamedItem("id")?.value;
+    const maybeSrc = this.attributes.getNamedItem("src")?.value;
+    const logDecorator =
+      maybeId != null ? `#${maybeId}` : maybeSrc != null ? `@${maybeSrc}` : "";
+
+    console[level](`[ScriptTs${logDecorator}]`, ...args);
+  }
+
+  removeImports = ScriptTs.removeImports;
+  removeTypes = ScriptTs.removeTypes;
+
+  hasSrc() {
+    return this.attributes.getNamedItem("src") != null;
+  }
+
   connectedCallback() {
-    this.extractSource().then((source) => ScriptTs.removeImports(source)).then((source) => {
-      const actualScript = document.createElement("script");
-      actualScript.text = tsBlankSpace(source);
-      this.textContent = "";
-      this.appendChild(actualScript);
-    });
+    this.log("debug", "connectedCallback");
+    // at this point we know the src attribute but the attributeChangedCallback will also fire
+    this.hasSrc() ||
+      this.extractSource().then((source) => {
+        this.updateScript(source);
+      });
+  }
+
+  attributeChangedCallback(name: string, oldValue: unknown, newValue: unknown) {
+    this.log("debug", `attribute = ${name}`, `${oldValue} -> ${newValue}`);
+    if (name === "src") {
+      this.extractSource().then((source) => {
+        this.updateScript(source);
+      });
+    }
   }
 
   async extractSource() {
     const maybeSrc = this.attributes.getNamedItem("src");
     if (maybeSrc != null) {
-      return (await fetch(`${maybeSrc.value}`)).text();
+      this.originalSource = await (await fetch(`${maybeSrc.value}`)).text();
     } else {
       await setTimeoutAsync();
-      return this.firstChild?.textContent ?? "";
+      this.originalSource = this.firstChild?.textContent ?? "";
     }
+    return this.originalSource;
   }
 
   static removeImports(source: string) {
-    return source.replaceAll("import", "// import");
+    return source
+      .replaceAll("import", "// import")
+      .replaceAll("export", "/* export */");
+  }
+
+  static removeTypes(source: string) {
+    return tsBlankSpace(source);
+  }
+
+  updateScript(source: string) {
+    try {
+      if (this.actualScript != null) {
+        this.removeChild(this.actualScript);
+      }
+      this.textContent = "";
+
+      source = ScriptTs.removeImports(source);
+      source = ScriptTs.removeTypes(source);
+      this.log("debug", "updateScript ", `source = ${source}`);
+
+      this.actualScript = document.createElement("script");
+      this.actualScript.text = source;
+      this.appendChild(this.actualScript);
+    } catch (e) {
+      this.log("error", "updateScript", e);
+      throw e;
+    }
   }
 }
 
